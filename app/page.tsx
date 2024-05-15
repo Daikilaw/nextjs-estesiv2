a0999c0464a24b17b50e7eef0035c34d5697f136
Adrian De Souza <67642190+Daikilaw@users.noreply.github.com>
2024-05-15 12:32:19 -0600

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -111,3 +111,23 @@ export default function Home() {
     </main>
   );
 }
+
+// Get the first 20 posts from WordPress, ordered by the date
+export async function getAllPostsFromWordPress(preview) {
+  const data = await fetchAPI(`
+    query AllPosts {
+      posts(first: 20, where: { orderby: { field: DATE, order: DESC } }) {
+        edges {
+          node {
+            title
+            excerpt
+            slug
+            date
+          }
+        }
+      }
+    }
+  `)
+
+  return data.posts
+}
